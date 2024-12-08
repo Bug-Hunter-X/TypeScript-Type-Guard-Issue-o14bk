95190ebe50ce0f36ae33e8d52f332cd731d8b0e9
BugHunter-X <askzenai@gmail.com>
2024-12-08 17:56:07 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,20 @@
+function printAll(args: any[]): void {
+  for (let i = 0; i < args.length; i++) {
+    console.log(args[i]);
+  }
+}
+
+function printAllWithTypeGuard(args: any[]): void {
+  for (let i = 0; i < args.length; i++) {
+    if (typeof args[i] === 'string') {
+      console.log(args[i]);
+    } else if (typeof args[i] === 'number') {
+      console.log(args[i]);
+    } else {
+      console.log('Unknown type');
+    }
+  }
+}
+
+printAll([1, 'hello', true, 2]);
+printAllWithTypeGuard([1, 'hello', true, 2]);
